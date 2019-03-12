d439e7304ed5add272b27c76c1d4ae48fd517a41
Fernando Salazar <fernandsalazar3@gmail.com>
2019-03-12 13:30:28 -0300

    init commit, served global route that return same text you send

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+
+const {
+  PORT = 8000
+} = process.env;
+
+// default middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Send back all the request with
+app.use('/', (req, res) => {
+  const text = req.body.text || req.query.text;
+
+  if (!text) {
+    res.status(400).json({
+      message: 'Missing prop \'text\' on body payload and query',
+    });
+  }
+
+  res.send(text);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
